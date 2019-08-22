8e0118e25de0ec2eb069a79aa1a996e1c3c791eb
Gui-Lima <ggfl@cin.ufpe.br>
2019-08-22 17:52:35 -0300

    Exercicio 1

--- calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
+++ calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
@@ -2,20 +2,39 @@ package br.ufpe.cin.android.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import java.lang.Exception
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    final val MATH_RESULT = "result"
+    final val MATH_CURRENT = "current"
+
+    private lateinit var edit_text : EditText
+    private lateinit var text_info : TextView
+
+    //Pega o saveState caso exista
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        setContentView(R.layout.activity_main_constraint)
+        edit_text = findViewById(R.id.text_calc)
+        text_info = findViewById(R.id.text_info)
+        val edit_textPreviousText = savedInstanceState?.get(MATH_CURRENT) ?: ""
+        edit_text.setText(edit_textPreviousText.toString())
+        val text_infoPreviousText = savedInstanceState?.get(MATH_RESULT) ?: ""
+        text_info.setText(text_infoPreviousText.toString())
 
+        setListenersOnButtons()
+    }
 
     //Como usar a função:
     // eval("2+2") == 4.0
     // eval("2+3*4") = 14.0
     // eval("(2+3)*4") = 20.0
     //Fonte: https://stackoverflow.com/a/26227947
+    //Fiz algumas mudanças para invés de jogar exceção, mostrar o dialog
     fun eval(str: String): Double {
         return object : Any() {
             var pos = -1
@@ -37,7 +56,10 @@ class MainActivity : AppCompatActivity() {
             fun parse(): Double {
                 nextChar()
                 val x = parseExpression()
-                if (pos < str.length) throw RuntimeException("Caractere inesperado: " + ch)
+                if (pos < str.length) {
+                    makeDialog("Caractere inesperado: " + ch)
+                    return -0.0
+                }
                 return x
             }
 
@@ -80,7 +102,10 @@ class MainActivity : AppCompatActivity() {
                     eat(')')
                 } else if ((ch in '0'..'9') || ch == '.') { // números
                     while ((ch in '0'..'9') || ch == '.') nextChar()
-                    x = java.lang.Double.parseDouble(str.substring(startPos, this.pos))
+                    try {x = java.lang.Double.parseDouble(str.substring(startPos, this.pos))}
+                    catch (e:Exception){
+                        return 0.0
+                    }
                 } else if (ch in 'a'..'z') { // funções
                     while (ch in 'a'..'z') nextChar()
                     val func = str.substring(startPos, this.pos)
@@ -93,14 +118,104 @@ class MainActivity : AppCompatActivity() {
                         x = Math.cos(Math.toRadians(x))
                     else if (func == "tan")
                         x = Math.tan(Math.toRadians(x))
-                    else
-                        throw RuntimeException("Função desconhecida: " + func)
+                    else{
+                        makeDialog("Função desconhecida: " + func)
+                        return -0.0
+                    }
                 } else {
-                    throw RuntimeException("Caractere inesperado: " + ch.toChar())
+                    makeDialog("Caractere inesperado: " + ch.toChar())
+                    return -0.0
                 }
                 if (eat('^')) x = Math.pow(x, parseFactor()) // potência
                 return x
             }
         }.parse()
     }
+
+    //Edita texto que mostra os números digitados de acordo com número digitado
+    override fun onClick(p0: View?) {
+        var buttonId = p0?.id
+        var clickedText = getChoosenButton(buttonId)
+        var text = if (clickedText.equals("")) "" else edit_text.text.toString().plus(clickedText)
+        edit_text.setText(text)
+    }
+
+    //Lida com mudança de config.
+    public override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putString(MATH_RESULT, text_info.text.toString())
+        savedInstanceState.putString(MATH_CURRENT, edit_text.text.toString())
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
+    //Recebe o id do botão apertado e decide oque fazer
+    fun getChoosenButton(buttonId: Int?) : String{
+        return when(buttonId) {
+            R.id.btn_0 -> "0"
+            R.id.btn_1 -> "1"
+            R.id.btn_2 -> "2"
+            R.id.btn_3 -> "3"
+            R.id.btn_4 -> "4"
+            R.id.btn_5 -> "5"
+            R.id.btn_6 -> "6"
+            R.id.btn_7 -> "7"
+            R.id.btn_8 -> "8"
+            R.id.btn_9 -> "9"
+            R.id.btn_Equal -> calculaSoma()
+            R.id.btn_Multiply -> "*"
+            R.id.btn_Divide -> "/"
+            R.id.btn_Add -> "+"
+            R.id.btn_Subtract -> "-"
+            R.id.btn_LParen -> "("
+            R.id.btn_RParen -> ")"
+            R.id.btn_Clear -> ""
+            R.id.btn_Dot -> "."
+            R.id.btn_Power -> "^"
+            else -> "masoq"
+        }
+    }
+
+    //Editar o textView do resultado da conta
+    private fun calculaSoma(): String {
+        text_info.setText(eval(edit_text.text.toString()).toString())
+        return ""
+    }
+
+    //Mostra a caixa de diálogo de erro
+    private fun makeDialog(message:String){
+        var alertDialog = AlertDialog.Builder(this)
+        alertDialog.setTitle("Alerta") // O Titulo da notificação
+        alertDialog.setMessage(message)
+        alertDialog.setView(R.layout.dialog_somethingwentwrong_fragment)
+
+        alertDialog.setPositiveButton("OK", { _, _ ->
+
+            Toast.makeText(this, "Sua conta foi ignorada e resultado zerado", Toast.LENGTH_LONG).show()
+        })
+
+        alertDialog.show()
+    }
+
+    //Coloca os listeners em todos os botões
+    private fun setListenersOnButtons() {
+        (findViewById(R.id.btn_0) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_1) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_2) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_3) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_4) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_5) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_6) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_7) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_8) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_9) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_Clear) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_Power) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_RParen) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_LParen) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_Equal) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_Dot) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_Add) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_Divide) as Button).setOnClickListener(this)
+        (findViewById(R.id.btn_Multiply) as Button).setOnClickListener(this)
+    }
+
 }
